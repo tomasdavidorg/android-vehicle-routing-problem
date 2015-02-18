4c989c2ec15e6497e3ee89abadbf35acb51b11eb
Tomas David <tdavid@redhat.com>
2015-02-18 15:23:51 +0100

    updated optaplanner domains from 6.3.0-SNAPSHOT part 2

---
+++ app/src/main/java/org/optaplanner/examples/vehiclerouting/domain/solver/nearby/CustomerNearbyDistanceMeter.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.examples.vehiclerouting.domain.solver.nearby;
+
+import org.optaplanner.core.impl.heuristic.selector.common.nearby.NearbyDistanceMeter;
+import org.optaplanner.examples.vehiclerouting.domain.Customer;
+import org.optaplanner.examples.vehiclerouting.domain.Standstill;
+import org.optaplanner.examples.vehiclerouting.domain.timewindowed.TimeWindowedCustomer;
+
+public class CustomerNearbyDistanceMeter implements NearbyDistanceMeter<Customer, Standstill> {
+
+    @Override
+    public double getNearbyDistance(Customer origin, Standstill destination) {
+        int distance = origin.getDistanceTo(destination);
+        // If arriving early also inflicts a cost (more than just not using the vehicle more), such as the driver's wage, use this:
+//        if (origin instanceof TimeWindowedCustomer && destination instanceof TimeWindowedCustomer) {
+//            distance += ((TimeWindowedCustomer) origin).getTimeWindowGapTo((TimeWindowedCustomer) destination);
+//        }
+        return distance;
+    }
+
+}
