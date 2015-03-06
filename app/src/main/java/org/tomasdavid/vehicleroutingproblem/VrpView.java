4694bac3bd4b6ba89c344b4319d41aff4cf74548
Tomas David <tdavid@redhat.com>
2015-03-06 19:01:24 +0100

    added vehicle routing problem view

---
+++ app/src/main/java/org/tomasdavid/vehicleroutingproblem/VrpView.java
@@ -0,0 +1,32 @@
+package org.tomasdavid.vehicleroutingproblem;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import org.optaplanner.examples.vehiclerouting.domain.VehicleRoutingSolution;
+
+public class VrpView extends View {
+
+    private VehicleRoutingSolution actualSolution;
+
+    private Context context;
+
+    public VrpView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+    }
+
+    public void setActualSolution(VehicleRoutingSolution actualSolution) {
+        this.actualSolution = actualSolution;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (this.actualSolution != null) {
+            new VrpPainter().paint(this.actualSolution, canvas, context.getResources());
+        }
+    }
+}
