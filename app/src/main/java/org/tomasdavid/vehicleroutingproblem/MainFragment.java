2573fd1cd1b97b520bbe5b371707991e7eb2ca92
Tomas David <tdavid@redhat.com>
2015-04-12 11:52:53 +0200

    main fragment refactoring

--- app/src/main/java/org/tomasdavid/vehicleroutingproblem/MainFragment.java
+++ app/src/main/java/org/tomasdavid/vehicleroutingproblem/MainFragment.java
@@ -2,39 +2,57 @@ package org.tomasdavid.vehicleroutingproblem;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+/**
+ * Main fragment with settings.
+ */
 public class MainFragment extends Fragment {
+
+    /**
+     * Default time limit in number picker.
+     */
+    private static final int DEFAULT_TIME_LIMIT = 10;
+
+    /**
+     * Minimum time limit in number picker.
+     */
+    private static final int MIN_TIME_LIMIT = 1;
+
+    /**
+     * Maximum time limit in number picker.
+     */
+    private static final int MAX_TIME_LIMIT = 1000;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-        Button button = (Button) view.findViewById(R.id.button_open_file);
-        button.setOnClickListener(new OpenFileButtonClick());
+
+        // time limit number picker initialization
         NumberPicker numberPicker = (NumberPicker) view.findViewById(R.id.timeLimitPicker);
-        numberPicker.setMinValue(1);
-        numberPicker.setMaxValue(1000);
-        numberPicker.setValue(1);
+        numberPicker.setMinValue(MIN_TIME_LIMIT);
+        numberPicker.setMaxValue(MAX_TIME_LIMIT);
+        numberPicker.setValue(DEFAULT_TIME_LIMIT);
         numberPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 
+        // welcome text
         TextView welcomeText = (TextView) view.findViewById(R.id.welcome_text);
         welcomeText.setText(Html.fromHtml(getString(R.string.welcome_text)));
 
+        // spinner for choosing algorithm initialization
         Spinner algSpinner = (Spinner) view.findViewById(R.id.algorithm_spinner);
-        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.algorithms, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.algorithms,
+                android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         algSpinner.setAdapter(spinnerAdapter);
 
@@ -45,46 +63,25 @@ public class MainFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_vrp, menu);
-        MenuItem playStopButton = menu.findItem(R.id.action_run);
-        playStopButton.setVisible(false);
+
+        // disable play button
+        menu.findItem(R.id.action_run).setVisible(false);
+
         super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
         if (id == R.id.action_about) {
             AboutAppDialog aad = new AboutAppDialog();
-            aad.show(getActivity().getSupportFragmentManager(), "");
+            aad.show(getActivity().getSupportFragmentManager(), null);
             return true;
         } else if (id == R.id.action_legend) {
-            LegendDialog aad = new LegendDialog();
-            aad.show(getActivity().getSupportFragmentManager(), "");
+            LegendDialog ld = new LegendDialog();
+            ld.show(getActivity().getSupportFragmentManager(), null);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
-    }
-
-    private class OpenFileButtonClick implements OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-
-            NumberPicker numberPicker = (NumberPicker) getActivity().findViewById(R.id.timeLimitPicker);
-            Spinner algSpinner = (Spinner) getActivity().findViewById(R.id.algorithm_spinner);
-
-            Bundle b = new Bundle();
-            b.putInt(VrpKeys.VRP_TIME_LIMIT.name(), numberPicker.getValue());
-            b.putString(VrpKeys.VRP_ALGORITHM.name(), getActivity().getResources().obtainTypedArray(R.array.algorithm_files).getString(algSpinner.getSelectedItemPosition()));
-
-            VrpFileListFragment a = new VrpFileListFragment();
-            a.setArguments(b);
-            FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.activity_main, a);
-            transaction.addToBackStack(null);
-            transaction.commit();
-        }
     }
 }
---
+++ app/src/main/java/org/tomasdavid/vehicleroutingproblem/OpenFileButton.java
@@ -0,0 +1,55 @@
+package org.tomasdavid.vehicleroutingproblem;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+
+/**
+ * Button for continue to vrp list fragment.
+ */
+public class OpenFileButton extends Button implements View.OnClickListener {
+
+    public OpenFileButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public OpenFileButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public OpenFileButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    /**
+     * Sets onClick listener.
+     */
+    private void init() {
+        this.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentActivity activity = (FragmentActivity) v.getContext();
+        NumberPicker numberPicker = (NumberPicker) activity.findViewById(R.id.timeLimitPicker);
+        Spinner algSpinner = (Spinner) activity.findViewById(R.id.algorithm_spinner);
+
+        // add setting values to bundle and starts new fragment
+        Bundle bundle = new Bundle();
+        bundle.putInt(VrpKeys.VRP_TIME_LIMIT.name(), numberPicker.getValue());
+        bundle.putString(VrpKeys.VRP_ALGORITHM.name(), activity.getResources().obtainTypedArray(R.array.algorithm_files)
+                .getString(algSpinner.getSelectedItemPosition()));
+        VrpFileListFragment fragment = new VrpFileListFragment();
+        fragment.setArguments(bundle);
+        activity.getSupportFragmentManager().beginTransaction().replace(R.id.activity_main, fragment)
+                .addToBackStack(null).commit();
+    }
+}
